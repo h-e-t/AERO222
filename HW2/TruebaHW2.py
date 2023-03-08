c49e177811b48c2224189a24e8210f6d2fabb586
h-e-t <98999909+h-e-t@users.noreply.github.com>
2023-03-08 08:59:10 -0600

    the updates for hw 2

--- HW2/TruebaHW2.py
+++ HW2/TruebaHW2.py
@@ -1,1 +1,24 @@
-print('Your mom is a bit of a hoe')
+from math import exp, tan, cos 
+
+def f(x): 
+    return tan(x) + exp(x) - x ** 2
+
+def fp(x):\
+    return 1 / cos(x)**2 + exp(x) - 2 * x
+
+# 1A
+def newton1A(f, fp):
+    resT = 10 ** -8
+    x0 = 0
+    xold = 0
+    res = f(x0)
+
+    count = 0
+    while abs(res) > resT:
+        xnew = xold - f(xold)/fp(xold)
+        res = f(xnew)
+        print(xnew)
+        xold = xnew
+        count += 1
+
+newton1A(f, fp)
